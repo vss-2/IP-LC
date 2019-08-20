335e878120539058f3cb09d5d4743de5d152eada
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-08-19 21:59:50 -0300

    Update QuickSort.hs

--- QuickSort.hs
+++ QuickSort.hs
@@ -1,4 +1,4 @@
--- Famosa implementação em 2 linhas
+-- Famosa implementação econômica em 2 linhas
 qsort [] = [] 
 qsort (x:xs) = qsort [y | y <- xs, y < x] ++ [x] ++ qsort [y | y <- xs, y >= x]
 
@@ -9,3 +9,17 @@ qsort (x:xs) = qsort [y | y <- xs, y < x] ++ [x] ++ qsort [y | y <- xs, y >= x]
 
 -- Input:  qsort[5,4,3,2,1]
 -- Output: [1,2,3,4,5]
+
+-- Versão menos econômica e mais humana (usando filter)
+quicksort [] = []
+quicksort (pivo:xs) = (quicksort menores) ++ [p] ++ (quicksort maiores)
+    where
+        menores = filter (< pivo) xs
+        maiores = filter (>= pivo) xs
+        
+-- Input:  qsort[10,9,8,7,6]
+-- Output: [6,7,8,9,10]
+
+-- O where classifica os padrões entre maiores e menores:
+-- onde o conjunto dos menores que pivo é filtrado para "menores"
+-- e o mesmo vale para o conjunto dos maiores.
