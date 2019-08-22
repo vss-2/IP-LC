29b0cf231c813042ec59bd6ebe1be773bd316d4d
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-08-21 21:59:43 -0300

    Update introducao.hs

--- introducao.hs
+++ introducao.hs
@@ -27,3 +27,53 @@ totalVendas :: Int -> Int -> Int
 totalVendas n s
   | (n == 0) = eIgual (vendas n) s
   | otherwise = eIgual (vendas n) s + totalVendas (n-1) s
+
+-----------------------------------------------------------
+-- Defina as seguintes funções (Fatorial, 4 iguais e quantos iguais):
+-- fat :: Int −> Int que calcula o fatorial de um inteiro dado
+-- como argumento
+-- all4Equal :: Int −> Int −> Int −> Int −> Bool que compara se
+-- quatro valores inteiros são iguais
+-- equalCount :: Int −> Int −> Int −> Int que retorna quantos
+-- argumentos são iguais
+
+fatorial :: Int -> Int
+fatorial 0 = 1
+fatorial 1 = 1
+fatorial n = n * fatorial (n-1)
+
+all4Equal :: Int -> Int -> Int -> Int -> Bool
+all4Equal x y z w
+  |((x == y) && (x == z) && (x == w) && (y == z) && (y == w) && (z == w)) = True
+  |otherwise = False
+
+equalCount :: Int -> Int -> Int -> Int
+equalCount x y z
+  |((x == y) && (x /= z) || (y == z) && (x /= z)) || (x == z) && (x /= y) = 2
+  |(y == z) && (y == x) = 3
+  |otherwise = 0
+
+
+-----------------------------------------------------------
+
+-- Uso de Where, Let In
+somaQuadradoWhereSimples :: Int -> Int -> Int
+somaQuadradoWhereSimples x y = qdx + qdy
+  where
+    qdx = x * x
+    qdy = y * y
+        
+        
+somaQuadradoWhereAvancado :: Int -> Int -> Int
+somaQuadradoWhereAvancado x y = quad x + quad y   
+  where 
+    quad z = z * z
+
+
+-- Como somar dois quadrados usando let 
+-- para definir funções e chamando no in
+somaQuadradoLetInt :: Int -> Int -> Int
+somaQuadradoLetInt x y =
+  let quadX = x * x 
+      quadY = y * y 
+  in quadX + quadY
