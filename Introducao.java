5bbbaf8455b919e0b44ba067ec60567967cfc0cb
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-11-05 23:17:48 -0300

    Update Introducao.java

--- Introducao.java
+++ Introducao.java
@@ -1,4 +1,4 @@
- // Podemos implementar uma Thread em java
+// Podemos implementar uma Thread em java
 // da seguinte forma:
 
 public class OlaMundoR implements Runnable
@@ -57,9 +57,45 @@ public class Espera extends Thread
 
 // O método sleep faz uma thread ser pausada.
 // Porém, isto pode causar uma InterruptedException
-public void sleep(long tempo){
-    
-}
+
+// public void sleep(long tempo){
+
+// }
 
 // Podemos provocar interrupções usando:
 // interrupt() e interrupted();
+
+//-------------------------------------------------
+//                 Exercicios
+//-------------------------------------------------
+
+/*
+1. Implemente um programa que calcula todos os
+números primos entre 1 e um valor N fornecido
+como argumento. Seu programa deve dividir o
+trabalho a ser realizado entre X threads (onde X
+também é uma entrada do programa) para tentar
+realizar o trabalho de maneira mais rápida que uma
+versão puramente sequencial.
+
+2. A thread principal do programa deve imprimir os
+números primos identificados apenas quando as
+outras threads terminarem.
+
+3. Construa um programa onde N threads incrementam
+contadores locais (não-compartilhados) em um laço. Cada
+thread imprime o valor de seu contador a cada incremento.
+As threads param quando seus contadores chegam a um
+valor limite X, recebido como entrada pelo programa.
+– Por que as saídas das threads se misturam?
+
+4. Agora mude seu programa para que as threads modifiquem
+um contador global compartilhado entre elas.
+– O que acontece com os resultados?
+
+5. Modifique o último programa que você construiu
+para que, a cada iteração, a thread espere 1ms. A
+thread que terminar a contagem primeiro (realizar
+todas as iterações) deve interromper todas as outras
+que estão executando.
+*/
