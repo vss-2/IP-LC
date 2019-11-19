3a94e57e27a05647c7f5dd5ab6fd09b8067d6f15
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-11-19 10:51:16 -0300

    Update Lanchonete.hs

--- Lanchonete.hs
+++ Lanchonete.hs
@@ -2,8 +2,8 @@ import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Concurrent.STM
 
--- Sua função: verifica se após fim (modificado pelas threads) chegou a zero. Encerra o loop caso sim, encerrando o main que encerra as threads
-
+-- Sua função: verifica se após fim (modificado pelas threads) chegou a zero.
+-- Encerra o loop caso sim, encerrando o main que encerra as threads
 waitThreads fim = do
     f <- takeMVar fim
     if (f > 0) then do
@@ -14,7 +14,7 @@ waitThreads fim = do
 
 
 -- Isso é tipo classe, setta as quantidades fornecidas e se chama
-fornecedor pao carne tomate fim = do
+produtor pao carne tomate fim = do
     fimAtual <- takeMVar fim
     atomically (do
         writeTVar pao 30
@@ -23,10 +23,10 @@ fornecedor pao carne tomate fim = do
     )
 
     putMVar fim (fimAtual-1)
-    fornecedor pao carne tomate fim
+    produtor pao carne tomate fim
 
 -- Isso é tipo classe, consome os produtos, e se chama de novo
-produtor pao carne tomate faca fim = do
+consumidor pao carne tomate faca fim = do
     -- Fim == quantida de execuções
     direitoFaca <- takeMVar faca
     fimAtual    <- takeMVar fim
@@ -45,7 +45,7 @@ produtor pao carne tomate faca fim = do
 
     putMVar faca direitoFaca
     putMVar fim (fimAtual-1)
-    produtor pao carne tomate faca fim
+    consumidor pao carne tomate faca fim
 
 
 main = do
@@ -55,16 +55,18 @@ main = do
     carne  <- atomically (newTVar qtdIng)
     tomate <- atomically (newTVar qtdIng)
 
-    -- Controla a concorrência (quantidade de execuções e o objeto de disputa: faca)
+    -- Controla a concorrência 
+    -- (quantidade de execuções e o objeto de disputa: faca)
     let qntExec = 20
     lockFaca <- newMVar 0
     qntExec = newMVar qntExec
 
     -- A partir de agora, pode-se iniciar a execução
 
-    -- Iniciando as threads: 1 produtora 2 fornecedoras
-    forkIO(fornecedor pao carne tomate qtdExec)
-    forkIO(produtor pao carne tomate lockFaca qtdExec)
-    forkIO(produtor pao carne tomate lockFaca qtdExec)
+    -- Iniciando as threads: 1 produtora, 2 consumidoras
+    forkIO(produtor pao carne tomate qtdExec)
+    forkIO(consumidor pao carne tomate lockFaca qtdExec)
+    forkIO(consumidor pao carne tomate lockFaca qtdExec)
 
-    -- O main chamou as threads, elas executam waitThreads e depois o código acaba
+    -- O main chamou as threads,
+    -- elas executam waitThreads e depois o código acaba
