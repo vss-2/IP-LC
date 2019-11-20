d96c7a78d70b9aa3c051d5e6ccd38e13b875e78c
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-11-20 00:25:44 -0300

    Update Polinomial.lua

--- Polinomial.lua
+++ Polinomial.lua
@@ -1,8 +1,4 @@
--- Não tá funcionando assim
-
-[[--
-tupla = {3,0,1}
-x = 10
+-- Implementação tosca, mas serve
 
 function newpoli(tupla)
 	return function(x)
@@ -10,19 +6,10 @@ function newpoli(tupla)
 		if x == 0 then resposta = tupla[3] end
 		if x == 1 then resposta = tupla[1] + tupla[2] + tupla[3] end
 		if x > 1 then resposta = tupla[1]*x*x + tupla[2]*x + tupla[3] end
-		print(resposta)
 		return resposta
 	end
 end
 
-print(resposta)
---]]
-
--- Tá funcionando assim
-tupla = {3,0,1}
-x = 10
-
-if x == 0 then resposta = tupla[3] end
-if x == 1 then resposta = tupla[1] + tupla[2] + tupla[3] end
-if x > 1 then resposta = tupla[1]*x*x + tupla[2]*x + tupla[3] end
-print(resposta)
+np = newpoli({3,0,1})
+print(np(5))
+print(np(10))
