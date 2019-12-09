51bc834637488270a985f5f0f1f761a90b78e213
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-12-09 16:34:38 -0300

    Update Final2019-1.hs
    Adicionando 1o letra A

--- Final2019-1.hs
+++ Final2019-1.hs
@@ -1,4 +1,14 @@
 -- 1a Questao
+-- Letra A
+retiraMultiplos :: Int -> [Int] -> [Int]
+retiraMultiplos 1 b = b
+retiraMultiplos a [] = []
+retiraMultiplos a (x:xs) = if (mod x a == 0) then retiraMultiplos a xs
+                           else x : retiraMultiplos a xs
+
+primos ::  [Int] -> [Int]
+primos [] = []
+primos (x:xs) = x : primos(retiraMultiplos x xs)
 
 -- Letra B
 ePrimo :: Int -> Bool
